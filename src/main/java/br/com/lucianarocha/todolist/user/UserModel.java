d61655f2775ac98456828b076fbde67d7b491911
Luciana Rocha <lucianajrocha97@gmail.com>
2023-10-10 10:47:51 -0300

    Initialize Project

---
+++ src/main/java/br/com/lucianarocha/todolist/user/UserModel.java
@@ -0,0 +1,21 @@
+package br.com.lucianarocha.todolist.user;
+
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public String getUsername(){
+        return this.username;
+
+    }
+
+}
