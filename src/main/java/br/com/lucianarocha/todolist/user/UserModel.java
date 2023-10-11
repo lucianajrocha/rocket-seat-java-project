486dc62c2d91afe18ffd14d1554888610966c570
Luciana Rocha <lucianajrocha97@gmail.com>
2023-10-11 11:45:27 -0300

    Segunda aula

--- src/main/java/br/com/lucianarocha/todolist/user/UserModel.java
+++ src/main/java/br/com/lucianarocha/todolist/user/UserModel.java
@@ -1,35 +1,30 @@
 package br.com.lucianarocha.todolist.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
 
-    public void setUsername(String username){
-        this.username = username;
-    }
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
-    public void setName (String name){
-        this.name = name;
-    }
-
-    public void setPassword(String password){
-        this.password = password;
-    }
-
-    public String getName(){
-
-        return this.name;
-    }
-
-    public String getPassword(){
-
-        return this.password;
-    }
-
-    public String getUsername(){
-        return this.username;
-
-    }
 
 }
